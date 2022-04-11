bed75797ca062089f7dd554c6551dc1f87786cc9
Juan David Bizerra Pimentel <juandbpimentel@alu.ufc.br>
2022-04-11 00:42:18 -0300

    TeladeProjetos,Criação de Projetos,Edição,Exclusão

--- costs-project/src/components/Pages/NewProject.jsx
+++ costs-project/src/components/Pages/NewProject.jsx
@@ -1,9 +1,40 @@
-const NewProject = () => {
-    return (
-        <div>
-            <h1>Novo Projeto</h1>
-        </div>
-    )
-}
+import { useHistory } from "react-router-dom";
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
 
-export default NewProject
+const NewProject = () => {
+	const history = useHistory();
+
+	function createPost(project) {
+		//  initialize cost and services
+		project.cost = 0;
+		project.services = [];
+
+		fetch("http://localhost:5000/projects", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(project),
+		})
+			.then((resp) => resp.json())
+			.then((data) => {
+				console.log(data);
+				//redirect
+				history.push("./projects", {
+					message: "Projeto criado com sucesso!",
+				});
+			})
+			.catch((err) => console.log(err));
+	}
+
+	return (
+		<div className={styles.newproject_container}>
+			<h1>Criar Projeto</h1>
+			<p>Crie seu projeto para depois adicionar os serviços</p>
+			<ProjectForm btnText="Criar Projeto" handleSubmit={createPost} />
+		</div>
+	);
+};
+
+export default NewProject;
